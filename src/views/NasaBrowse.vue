0388e1b8520b3525bc46d5b7b7ee6c0f88567f34
senior0023 <jun.jie0305@protonmail.com>
2020-10-14 21:11:56 -0700

    search neo by id initial commit

--- src/views/NasaBrowse.vue
+++ src/views/NasaBrowse.vue
@@ -4,6 +4,12 @@
       NASA Browse - NASA NEO(Nearth Earth Object)
     </h2>
     <p v-if="showErrorMsg" class="error">{{ showErrorMsg }}</p>
+    <!-- get Neo feed by id -->
+    <div class="neo-id-container">
+      <label for="neo_id">Neo ID:</label>
+      <input v-model.trim="neo_id" type="text" placeholder="32144234" id="nedo_id" />
+      <button @click="searchNeoById()" class="button search">Search</button>
+    </div>
     <!-- table part -->
     <div class="table">
       <table class="styled-table">
@@ -38,7 +44,8 @@
         datas: [],
         loading: false,
         api_key: 'eIwAfFIAzJ8ChGli7AQGapj9CnLjZCVUoUYMNH5S',
-        showErrorMsg: null
+        showErrorMsg: null,
+        neo_id: null,
       }
     },
     computed() {
@@ -70,10 +77,42 @@
           .finally(() => {
             this.loading = false
           })
+      },
+      searchNeoById() {
+        this.datas = []
+        // set loading spinner
+        this.loading = true
+
+        let params = {
+          api_key: this.api_key
+        }
+        this.axios.get('https://api.nasa.gov/neo/rest/v1/neo/' + this.neo_id, {params: params})
+          .then((response) => {
+            this.datas.push(response)
+          })
+          .catch(err => {
+            this.showErrorMsg = err.response.data.error_message
+          })
+          .finally(() => {
+            this.loading = false
+          })
       }
     }
   }
 </script>
 
 <style lang="scss" scoped>
+  .neo-id-container {
+    display: flex;
+    flex-grow: 1;
+
+    label {
+      width: 100px;
+      line-height: 3rem;
+    }
+
+    .search {
+      margin-bottom: 1rem;
+    }
+  }
 </style>
